1c7f704b6dce33c84f251d97976dcc832cbb783f
Radu-Branzei <115561060+Radu-Branzei@users.noreply.github.com>
2023-07-11 09:31:56 +0300

    Modified category model.

--- src/models/category.model.ts
+++ src/models/category.model.ts
@@ -1,16 +1,15 @@
 export class Category {
-  ID!: string[] ;
-  Name!: string[] ;
-  productList!: string[] ;
+	ID!: string;
+	Name!: string;
+	productList!: string;
 
-  constructor(category?: Partial<Category>) {
-    if (!category || !(category instanceof Object)) {
-      category = <Category>(<any>{});
-    }
+	constructor(category?: Partial<Category>) {
+		if (!category || !(category instanceof Object)) {
+		category = <Category>(<any>{});
+		}
 
-    this.ID = category.ID || "";
-	this.Name = category.Name || "";
-	this.productList = category.productList || "";
-	  
-  }
+		this.ID = category.ID || "";
+		this.Name = category.Name || "";
+		this.productList = category.productList || "";
+	}
 }
