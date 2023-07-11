56b589207ff4d102d6703d75bbc4be95e2f75684
Lauraa980 <lmarteniuc24@gmail.com>
2023-07-11 09:29:41 +0200

    Model Change

--- src/models/category.model.ts
+++ src/models/category.model.ts
@@ -1,15 +1,16 @@
 export class Category {
+  ID!: string[] ;
+  Name!: string[] ;
+  productList!: string[] ;
 
-	ID!: string;
-	Name!: string;
-   
-	constructor(category?: Partial<Category>) {
+  constructor(category?: Partial<Category>) {
+    if (!category || !(category instanceof Object)) {
+      category = <Category>(<any>{});
+    }
 
-		if (!category || !(category instanceof Object)) {
-		    category = <Category><any>{};
-		}
-	
-		this.ID = category.ID || "";
-		this.Name = category.Name || "";
-	}
+    this.ID = category.ID || "";
+	this.Name = category.Name || "";
+	this.productList = category.productList || "";
+	  
+  }
 }
