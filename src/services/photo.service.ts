3089bd09f87057b93cea43588c81eb247511c131
FitaRobert <fita.robert@yahoo.com>
2023-07-12 09:43:30 +0300

    update resolved

--- src/services/photo.service.ts
+++ src/services/photo.service.ts
@@ -33,6 +33,7 @@ async function savePhoto(photo: Partial<Photo>): Promise<Error | Photo> {
 }
 
 async function getPhotos(): Promise<Error | Photo[]> {
+
   try {
     const photos = await PhotoDb.find<Photo>({});
     return photos;
@@ -49,25 +50,29 @@ async function deletePhoto(id: ObjectId): Promise<Error | Photo | null> {
     } catch (ex: any) {
         return Error(ex.message);
     }
+
 }
 
 async function updatePhoto(body:Photo): Promise<Error | Photo | null> {
 
+    try{
+      const photo = await PhotoDb.findById(body.id);
 
-    try {
-        const photo =await PhotoDb.findByIdAndUpdate<Photo>({id:body.id},
-            {
-                photoLink:body.photoLink,
-                description:body.description
-            }
-            );    
-        return photo;
+      if(!photo){
+        return Error("Photo not found");
+      }
+      
+      photo.photoLink=body.photoLink;
+      photo.description=body.description;
+
+      await photo.save();
+
+      return photo;
     }
 
     catch (ex: any) {
-        return Error(ex.message);
+      return new Error(ex.message);
     }
-
 }
     
 
