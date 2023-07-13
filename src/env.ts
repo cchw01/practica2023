9ad7c34d08b56eb03e1a38ce6f0601bdfe9538a4
NicuDan <nicudancostin@gmail.com>
2023-07-13 09:47:39 +0300

    Done

--- src/env.ts
+++ src/env.ts
@@ -5,16 +5,18 @@ function getEnv() {
   return {
     PORT: 80,
 
-    NODE_ENV: "development",
+    NODE_ENV: 'development',
 
-    DISCOVERY_CLIENT_ROUTE: "/discovery/client",
-    USER_ROUTE: "/api/user",
-    USER_MANAGEMENT: "/user",
-    PHOTO_ROUTE: "/photo",
+    DISCOVERY_CLIENT_ROUTE: '/discovery/client',
+    USER_ROUTE: '/api/user',
+    USER_MANAGEMENT: '/user',
+    PHOTO_ROUTE: '/photo',
     ReviewRestaurant_MANAGEMENT: '/reviewRestaurant',
 
-    MONGO_URL: "mongodb://127.0.0.1:27017",
-    DB_NAME: "restaurantDB",
-    MAIN_ENDPOINT: "/",
+    MONGO_URL: 'mongodb://127.0.0.1:27017',
+    DB_NAME: 'restaurantDB',
+    USER_DB: 'userDB',
+    reviewRestaurantDB: 'reviewRestaurantDB',
+    MAIN_ENDPOINT: '/',
   };
 }
