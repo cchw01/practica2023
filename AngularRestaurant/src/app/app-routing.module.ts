74fcc7df927ebe855c72b0c50d5aeca83804b0cd
CezarPetreanu <cezarpetreanu4013@gmail.com>
2023-07-20 21:57:03 +0300

    Main commit. Home page content.

--- AngularRestaurant/src/app/app-routing.module.ts
+++ AngularRestaurant/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './templates/home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: HomePageComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+export const RoutingComponents = {
+  HomePageComponent,
+};
