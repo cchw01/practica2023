58115fbc4f77b5c46c02c7b08727958435869d9e
CezarPetreanu <cezarpetreanu4013@gmail.com>
2023-07-21 09:58:48 +0300

    added angular material. delete old modules

--- AngularRestaurant/src/app/app-routing.module.ts
+++ AngularRestaurant/src/app/app-routing.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomePageComponent } from './templates/home-page/home-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 const routes: Routes = [
-  {path: '', component: HomePageComponent},
-  
+  { path: '', component: HomePageComponent },
+
   //to modify component for the rest of the pages
 
-  {path: 'menu', component: HomePageComponent},
-  {path: 'orders', component: HomePageComponent},
-  {path: 'reservations', component: HomePageComponent},
-  {path: 'contact', component: HomePageComponent},
-  {path: 'reviewRestaurant', component: HomePageComponent},
-  {path: 'login', component: HomePageComponent},
+  { path: 'menu', component: HomePageComponent },
+  { path: 'orders', component: HomePageComponent },
+  { path: 'reservations', component: HomePageComponent },
+  { path: 'contact', component: HomePageComponent },
+  { path: 'reviewRestaurant', component: HomePageComponent },
+  { path: 'login', component: HomePageComponent },
 ];
 
 @NgModule({
