69b0507b88726bdc576a2f658dfb48bc212860c0
Stefan Chiper <stefan814759@gmail.com>
2023-07-21 10:06:10 +0300

    login 2.0

--- AngularRestaurant/src/app/app-routing.module.ts
+++ AngularRestaurant/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+export const RoutingComponents = [LoginComponent];
