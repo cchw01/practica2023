450b43043281beca39a91910f0ec1c4a18877d65
raluca47 <raluca987123@gmail.com>
2023-07-21 11:52:25 +0300

    finished navbar

--- AngularRestaurant/src/app/app-routing.module.ts
+++ AngularRestaurant/src/app/app-routing.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './templates/home-page/home-page.component';
+import { MenuComponent } from './menu/menu.component';
 
 const routes: Routes = [
   {path: '', component: HomePageComponent},
   
   //to modify component for the rest of the pages
 
-  {path: 'menu', component: HomePageComponent},
+  {path: 'menu', component: MenuComponent},
   {path: 'orders', component: HomePageComponent},
   {path: 'reservations', component: HomePageComponent},
   {path: 'contact', component: HomePageComponent},
   {path: 'reviewRestaurant', component: HomePageComponent},
   {path: 'login', component: HomePageComponent},
+  {path: 'register', component: HomePageComponent},
 ];
 
 @NgModule({
@@ -22,4 +24,5 @@ const routes: Routes = [
 export class AppRoutingModule {}
 export const RoutingComponents = {
   HomePageComponent,
+  MenuComponent
 };
--- AngularRestaurant/src/app/app.module.ts
+++ AngularRestaurant/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MenuComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
