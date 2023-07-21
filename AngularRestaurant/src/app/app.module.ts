4275f8512356ecb355266013b2a70afb3e2ea97f
CezarPetreanu <cezarpetreanu4013@gmail.com>
2023-07-21 10:00:27 +0300

    added angular material. deleted old imports

--- AngularRestaurant/src/app/app.module.ts
+++ AngularRestaurant/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, MatButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
