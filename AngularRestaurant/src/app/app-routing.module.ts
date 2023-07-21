9d9b2d5f3718d31115f8faaeb5356179421d88b6
Muntean Razvan <munteanr20@gmail.com>
2023-07-21 11:49:37 +0300

    Register page

--- AngularRestaurant/src/app/app-routing.module.ts
+++ AngularRestaurant/src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
-export const RoutingComponents = [LoginComponent];
+export class AppRoutingModule { }
+export const RoutingComponents = [LoginComponent,
+  RegisterComponent];
--- AngularRestaurant/src/app/app.module.ts
+++ AngularRestaurant/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
