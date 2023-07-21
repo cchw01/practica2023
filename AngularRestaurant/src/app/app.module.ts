19600e4a21bdc5dd64022a28358a6c1249cf3478
raluca47 <raluca987123@gmail.com>
2023-07-21 12:22:47 +0300

    conflicts

--- AngularRestaurant/src/app/app.module.ts
+++ AngularRestaurant/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, platformBrowser } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,8 +8,7 @@ import { MenuComponent } from './menu/menu.component';
 @NgModule({
   declarations: [AppComponent, MenuComponent],
   imports: [BrowserModule, AppRoutingModule],
+  bootstrap : [AppComponent],
 })
-
-
 
 export class AppModule {}
