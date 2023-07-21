2444c9fe35a70323b3d5431a84207ac715ef5e9d
Radu-Branzei <115561060+Radu-Branzei@users.noreply.github.com>
2023-07-21 11:09:10 +0300

    Initial menu.

--- AngularRestaurant/src/app/menu/menu.component.ts
+++ AngularRestaurant/src/app/menu/menu.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuService } from '../services/menu.service';
+import { Product } from '../interfaces/product';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
 
+  productList!: Product[];
+
+  constructor(private productMenuService: MenuService) { }
+
+  ngOnInit(): void {
+    this.productMenuService.getProductList();
+    //console.log(this.productList);
+  }
 }
--- AngularRestaurant/src/app/services/menu.service.ts
+++ AngularRestaurant/src/app/services/menu.service.ts
@@ -1,17 +1,27 @@
 import { Injectable, OnInit } from '@angular/core';
 import { Product } from '../interfaces/product';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MenuService implements OnInit {
 
-  httpClient: string = "http://localhost:80/product";
-  productList!: Product[];
+  http: string = "http://localhost:80";
+  productList!: Observable<Product[]>;
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {
+    this.httpClient.get<Product[]>(this.http + "/product").subscribe((x) => {
+      console.log("ssadasdads");
+    });
+   }
 
   ngOnInit(): void {
-      
+    
+  }
+
+  getProductList(): Observable<Product[]> {
+    return this.productList;
   }
 }
