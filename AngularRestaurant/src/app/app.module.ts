0eea1a1dac6d732d9930053816dd1e8c75e7b644
raluca47 <raluca987123@gmail.com>
2023-07-24 10:25:49 +0300

    revert ok

--- AngularRestaurant/src/app/app.module.ts
+++ AngularRestaurant/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule, platformBrowser } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule],
