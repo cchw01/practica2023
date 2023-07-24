53ffc561fcdd5d98d34dcd4735bbbb746ccdec8a
raluca47 <raluca987123@gmail.com>
2023-07-24 10:06:09 +0300

    added some modules

--- AngularRestaurant/src/app/app.module.ts
+++ AngularRestaurant/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule, platformBrowser } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavBarComponent],
   imports: [BrowserModule, AppRoutingModule],
   bootstrap : [AppComponent],
 })
