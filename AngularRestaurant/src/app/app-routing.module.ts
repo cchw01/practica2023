44ba4b7aa439c3f08ecb7a7298d97a0c7c96e521
rocape <Cristian_Apetroaie@waters.com>
2023-07-24 11:41:37 +0300

    modified all reviews

--- AngularRestaurant/src/app/app-routing.module.ts
+++ AngularRestaurant/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RestaurantReviewPageComponent } from './templates/restaurant-review-page/restaurant-review-page.component';
+import { AllReviewsComponent } from './templates/all-reviews/all-reviews.component';
 
 const routes: Routes = [
+  {path:'allReviews', component:AllReviewsComponent},
   {path:'review', component:RestaurantReviewPageComponent}
 ];
 
